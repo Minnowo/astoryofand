7c0b97c9ee39649d50c41ea90f60ce7fb55770f5
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-12-30 18:39:56 -0500

    dynamic entry for different payment / delivery

--- orders/order.go
+++ orders/order.go
@@ -3,17 +3,18 @@ package orders
 import "minno/astoryofand/assets"
 
 type Order struct {
-	Email           string  `json:"email" form:"email"`
-	PayMethod       string  `json:"paymethod" form:"paymethod"`
-	BoxSetCount     uint32  `json:"boxsetcount" form:"boxsetcount"`
-	StickerCount    uint32  `json:"stickercount" form:"stickercount"`
-	TotalCost       float32 `json:"totalcost"`
-	FullName        string  `json:"fullname" form:"fullname"`
-	DeliveryMethod  string  `json:"deliverymethod" form:"deliverymethod"`
-	DeliveryAddress string  `json:"deliveryaddress" form:"deliveryaddress"`
-	ShippingAddress string  `json:"shippingaddress" form:"shippingaddress"`
-	City            string  `json:"city" form:"city"`
-	ZipCode         string  `json:"zipcode" form:"zipcode"`
+	Email          string  `json:"email" form:"email"`
+	PayMethod      string  `json:"paymethod" form:"paymethod"`
+	BoxSetCount    uint32  `json:"boxsetcount" form:"boxsetcount"`
+	StickerCount   uint32  `json:"stickercount" form:"stickercount"`
+	TotalCost      float32 `json:"totalcost"`
+	FullName       string  `json:"fullname" form:"fullname"`
+	DeliveryMethod string  `json:"deliverymethod" form:"deliverymethod"`
+	Address        string  `json:"address" form:"address"`
+	City           string  `json:"city" form:"city"`
+	ZipCode        string  `json:"zipcode" form:"zipcode"`
+	OtherDelivery  string  `json:"otherdelivery" form:"otherdelivery"`
+	OtherPay       string  `json:"otherpay" form:"otherpay"`
 }
 
 func CheckValidOrder(o Order) bool {
