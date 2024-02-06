9effc8fd086f1c863dd3c36b2e46019c3b2684ad
Minnowo <62451415+Minnowo@users.noreply.github.com>
2024-02-06 12:29:06 -0500

    Changed price to 35; TODO make this not compiled in

--- assets/constants.go
+++ assets/constants.go
@@ -3,5 +3,5 @@ package assets
 const PGPOutputDir string = "./enc/pgp_out"
 const UsesOutputDir string = "./enc/uses"
 
-const BoxSetPrice float32 = 43.75
+const BoxSetPrice float32 = 35.0
 const StickerCost float32 = 2.0
