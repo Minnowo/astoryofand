967f952b47ce10f4b3c16364aa77b0ca552feaa4
Minnowo <62451415+Minnowo@users.noreply.github.com>
2024-02-07 08:58:10 -0500

    Forgot this alt text

--- view/components/header_templ.go
+++ view/components/header_templ.go
@@ -26,7 +26,7 @@ func Navbar(title, brandName string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"navbar\"><div class=\"navbar-collapse nav_pad\"><a class=\"navbar-brand container-transition\" href=\"/\"><img src=\"/static/favicon.png\" height=\"64\"> ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav class=\"navbar\"><div class=\"navbar-collapse nav_pad\"><a class=\"navbar-brand container-transition\" href=\"/\"><img src=\"/static/favicon.png\" height=\"64\" alt=\"A Story of And Logo\"> ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
